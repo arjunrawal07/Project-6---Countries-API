c6507d0b5115a93bb8166494e3d9f9800a3c8f3e
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-04-15 10:52:13 -0400

    set up create route and tested

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const app = express();
-// const mongoose = require("mongoose");
 const router = require("./routes/country");
 const Country = require("./models/Country");
-// const axios = require("axios").default;
 const parser = require("body-parser");
-
-app.use(parser.json());
+const app = express();
 app.use(router);
+app.use(parser.json());
 
 app.get("/", (req, res) => {
   Country.find({}).then((countries) => {
@@ -15,19 +12,19 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/name/:name", (req, res) => {
+app.get("/countries/:name", (req, res) => {
   Country.find({ name: req.params.name }).then((country) => {
     res.json(country);
   });
 });
 
 app.post("/country", (req, res) => {
-  Country.create(req.body).then((newCountry) => {
-    res.json(newCountry);
+  console.log(req.body);
+  Country.create(req.body).then((country) => {
+    console.log("Successfully created", country);
+    res.json(country);
   });
 });
-
-// app.post("/country/:name/")
 
 app.listen(4000, () => {
   console.log("I'm listening on 4000!");
